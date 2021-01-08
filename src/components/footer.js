cac3a04b06ddd91893c93e70206d63d3bd0cddf2
Jen <jen@grid.app>
2021-01-08 10:58:00 +0100

    fix link

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <div className={sass.footer}>
             <div className={sass.footerInner}>
-                <p>Developed by Jen Palacios with <a href="https://www.gatsbyjs.org" target="_blank">Gatsby.js</a>. The cool icons come from <a href="" target="_blank">Ego icons</a> and the dope patterns from <a href="http://www.heropatterns.com/" target="_blank">Hero Patterns</a>.</p>
+                <p>Developed by Jen Palacios with <a href="https://www.gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby.js</a>. The cool icons come from <a href="https://ego.streamlineicons.com/" target="_blank" rel="noopener noreferrer">Ego icons</a> and the dope patterns from <a href="https://www.heropatterns.com/" target="_blank" rel="noopener noreferrer">Hero Patterns</a>.</p>
                 <p className={sass.footerYear}>{thisYear}</p>
             </div>
         </div>
