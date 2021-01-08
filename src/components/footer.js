5671e698f8e967277ed6aa0d41c3c15f0a5d6284
Jen <jen@grid.app>
2021-01-08 10:45:59 +0100

    add footer again

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,13 @@ import React from 'react';
 import sass from '../styles/index.module.sass';
 
 const Footer = () => {
+    const thisYear = new Date().getFullYear();
+    
     return (
         <div className={sass.footer}>
             <div className={sass.footerInner}>
-            <p>Made by Jen Palacios with <a href="https://www.gatsbyjs.org" target="_blank">Gatsby.js</a>. The cool icons come from <a href="" target="_blank">Ego icons</a> and the dope patterns from <a href="http://www.heropatterns.com/" target="_blank">Hero Patterns</a>.</p>
+                <p>Developed by Jen Palacios with <a href="https://www.gatsbyjs.org" target="_blank">Gatsby.js</a>. The cool icons come from <a href="" target="_blank">Ego icons</a> and the dope patterns from <a href="http://www.heropatterns.com/" target="_blank">Hero Patterns</a>.</p>
+                <p className={sass.footerYear}>{thisYear}</p>
             </div>
         </div>
     );
